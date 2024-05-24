22a69eb04e1add6d8fe8eb78c7c0cdf885622f1e
Faddellin <belovodchenko2005@mail.ru>
2024-05-25 00:48:29 +0700

    Affine bug fixed

--- app/src/main/java/com/example/photoeditor/Affine.kt
+++ app/src/main/java/com/example/photoeditor/Affine.kt
@@ -4,22 +4,12 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.view.MotionEvent
 import android.widget.ImageView
-import androidx.lifecycle.lifecycleScope
 import com.example.photoeditor.AffineTransform.AffineTransform
 import com.example.photoeditor.Cube3d.Tria3d
 import com.example.photoeditor.Cube3d.vec3d
 import com.example.photoeditor.UsefulFuns.createColor
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
-import kotlin.math.PI
-import kotlin.math.ceil
 import kotlin.math.max
 import kotlin.math.min
-import kotlin.math.sin
 import kotlin.math.sqrt
 
 
@@ -27,51 +17,51 @@ class Affine {
     companion object {
         var radius = 0.0f
         val rgb = arrayOf(createColor(255,0,0,255), createColor(0,255,0,255), createColor(0,0,255,255))
-        var curPoints: Array<vec2d> =
-            arrayOf(vec2d(0.0f, 0.0f), vec2d(0.0f, 0.0f), vec2d(0.0f, 0.0f))
         var curIndex = 0
-        var curTriangle: Int = 1
-        var affineTriangle: Tria3d = Tria3d(
-            vec3d(1.0f, 1.0f, 0.0f),
-            vec3d(2.0f, 2.0f, 0.0f),
-            vec3d(2.0f, 3.0f, 0.0f),
-            vec2d(1.0f, 1.0f),
-            vec2d(2.0f, 2.0f),
-            vec2d(2.0f, 3.0f)
-        )
+        var curTriangle: Int = -1
+        lateinit var affineTriangle: Tria3d
 
          fun addPoint(x: Float, y: Float, imageView: ImageView, bitmap:Bitmap) {
-            val bitmapWithPoint = bitmap.copy(bitmap.config,true)
+             if (curTriangle != -1){
+                 val bitmapWithPoint = bitmap.copy(bitmap.config,true)
 
-            if (curTriangle == 1) {
-                affineTriangle.t[curIndex] = vec2d(x, y)
-                for(pointIndex in 0..2){
-                    if(affineTriangle.t[pointIndex].x == -1.0f){
-                        continue
-                    }
+                 if (curTriangle == 1) {
+                     affineTriangle.t[curIndex] = vec2d(x, y)
+                     for(pointIndex in 0..2){
+                         if(affineTriangle.t[pointIndex].x == -1.0f){
+                             continue
+                         }
+                         createPoint(affineTriangle.t[pointIndex],bitmapWithPoint,rgb[pointIndex])
+                     }
+                 } else {
+                     affineTriangle.p[curIndex] = vec3d(x, y, 0.0f)
+                     for(pointIndex in 0..2){
+                         if(affineTriangle.p[pointIndex].x == -1.0f){
+                             continue
+                         }
+                         createPoint(affineTriangle.p[pointIndex],bitmapWithPoint,rgb[pointIndex])
+                     }
 
-                    createPoint(affineTriangle.t[pointIndex],bitmapWithPoint,rgb[pointIndex])
-                }
-            } else {
-                affineTriangle.p[curIndex] = vec3d(x, y, 0.0f)
-                for(pointIndex in 0..2){
-                    if(affineTriangle.p[pointIndex].x == -1.0f){
-                        continue
-                    }
-                    createPoint(affineTriangle.p[pointIndex],bitmapWithPoint,rgb[pointIndex])
-                }
+                 }
+                 imageView.setImageBitmap(bitmapWithPoint)
+                 curIndex++
+                 if (curIndex == 3) {
+                     curIndex = 0
+                 }
+             }
 
-            }
-            imageView.setImageBitmap(bitmapWithPoint)
-            curIndex++
-            if (curIndex == 3) {
-                curIndex = 0
-            }
         }
 
         @SuppressLint("ClickableViewAccessibility")
         fun ImageView.setTouchable(imageView: ImageView, bitmap: Bitmap) {
-            radius = bitmap.width.toFloat()
+            radius = bitmap.width.toFloat()/2
+            affineTriangle =  Tria3d(vec3d(-1.0f, -1.0f, 0.0f),
+                                     vec3d(-1.0f, -1.0f, 0.0f),
+                                     vec3d(-1.0f, -1.0f, 0.0f),
+                                     vec2d(-1.0f, -1.0f),
+                                     vec2d(-1.0f, -1.0f),
+                                     vec2d(-1.0f, -1.0f))
+
             imageView.setOnTouchListener { v, event ->
                 if (event.action == MotionEvent.ACTION_DOWN) {
                     val x = bitmap.width * (event.x) / imageView.width
@@ -132,14 +122,21 @@ class Affine {
 
         suspend fun callAffine(bitmap: Bitmap): Bitmap {
 
-            affineTriangle.p[0].y = bitmap.height - affineTriangle.p[0].y
-            affineTriangle.p[1].y = bitmap.height - affineTriangle.p[1].y
-            affineTriangle.p[2].y = bitmap.height - affineTriangle.p[2].y
-            affineTriangle.t[0].y = bitmap.height - affineTriangle.t[0].y
-            affineTriangle.t[1].y = bitmap.height - affineTriangle.t[1].y
-            affineTriangle.t[2].y = bitmap.height - affineTriangle.t[2].y
+            val finalTransformedTriangle =  Tria3d(vec3d(affineTriangle.p[0].x, affineTriangle.p[0].y, 0.0f),
+                                                   vec3d(affineTriangle.p[1].x, affineTriangle.p[1].y, 0.0f),
+                                                   vec3d(affineTriangle.p[2].x, affineTriangle.p[2].y, 0.0f),
+                                                   vec2d(affineTriangle.t[0].x, affineTriangle.p[0].y),
+                                                   vec2d(affineTriangle.t[1].x, affineTriangle.p[1].y),
+                                                   vec2d(affineTriangle.p[2].x, affineTriangle.p[2].y))
 
-            val newAffine = AffineTransform(affineTriangle)
+            finalTransformedTriangle.p[0].y = bitmap.height - affineTriangle.p[0].y
+            finalTransformedTriangle.p[1].y = bitmap.height - affineTriangle.p[1].y
+            finalTransformedTriangle.p[2].y = bitmap.height - affineTriangle.p[2].y
+            finalTransformedTriangle.t[0].y = bitmap.height - affineTriangle.t[0].y
+            finalTransformedTriangle.t[1].y = bitmap.height - affineTriangle.t[1].y
+            finalTransformedTriangle.t[2].y = bitmap.height - affineTriangle.t[2].y
+
+            val newAffine = AffineTransform(finalTransformedTriangle)
             return newAffine.process(bitmap)
         }
 
